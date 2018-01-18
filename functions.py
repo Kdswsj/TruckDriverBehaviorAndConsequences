962ba0f180aa252155c0d93ad6b7071bd78ed47b
Kim <kimdsorensen@gmail.com>
2018-01-18 15:24:07 -0600

    updated functions

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.linear_model import LinearRegression
 
 def GetData():
     # read in the data
@@ -21,4 +22,24 @@ def GetData():
     df = df[df['DriveFuelEconomy'] < 9]
 
     #return dataset for analysis
-    return df 
+    return df
+
+def AdjustDFE(df):
+    #select the features
+    X = df[['DistanceAvgLoadFactor', 'StopIdlePercent', 'DistanceAvgvehiclespeed','TopGearMinusOneUsage', 'HardBrakesPr1000miles', \
+           'BrakesPr1000miles', 'IdleFuelPercentage', 'EngineBrakePercentage', 'CruisePercentage', 'OverSpeedBPercentage', \
+           'OverSpeedAPercentage', 'EngineFanPercentage']]
+    y = df['DriveFuelEconomy']
+
+    lm = LinearRegression(normalize=True)
+    lm.fit(X,y)
+
+    # replace DistanceAvgLoadFactor with AvgLoadFactor (45%)
+    X = df[['AvgLoadFactor', 'StopIdlePercent', 'DistanceAvgvehiclespeed','TopGearMinusOneUsage', 'HardBrakesPr1000miles', \
+           'BrakesPr1000miles', 'IdleFuelPercentage', 'EngineBrakePercentage', 'CruisePercentage', 'OverSpeedBPercentage', \
+           'OverSpeedAPercentage', 'EngineFanPercentage']]
+
+    # calculate dfe45
+    dfe45 = lm.predict(X)
+
+    return dfe45
